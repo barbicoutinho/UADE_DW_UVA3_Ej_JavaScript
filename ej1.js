73573d2d1e1c88dcc130535834e5971c8b996372
Barbara Coutinho <105075164+barbicoutinho@users.noreply.github.com>
2025-05-13 12:17:03 +0100

    Create ej1.js

---
+++ ej1.js
@@ -0,0 +1,8 @@
+// Datos fijos
+let nombre = "Lucía";
+let apellido = "Gómez";
+let ciudad = "Mendoza";
+
+// Mostrar mensaje de bienvenida
+document.getElementById("mensaje").textContent =
+  `¡Bienvenida ${nombre} ${apellido} desde ${ciudad}!`;
